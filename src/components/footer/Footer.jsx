1246e0a44b1210ca4a5062056c15461d68ea0b9f
Miguel Valbuena <mavalbuenaol@nh.inet>
2024-04-18 16:43:43 -0500

    add complaint-box link

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,14 +1,18 @@
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import "../../styles/components/footer/footer.css"
 
 const Footer = () => {
-  return (
-    <div className="footer">
-      <Link className="link" to="/about-us">
-        Sobre nosotros
-      </Link>
-    </div>
-  );
+    return (
+        <div className="footer">
+            <Link className="link" to="/about-us">
+                Sobre nosotros
+            </Link>
+            <p> | </p>
+            <Link className="link" to="/complaint-box">
+                Buzón de quejas
+            </Link>
+        </div>
+    );
 };
 
 export default Footer;
